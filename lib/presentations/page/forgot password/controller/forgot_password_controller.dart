d40d9e277ba6189f4d0feb9e25bc8253aaf671db
Naisya Yuen <naisyayr09@gmail.com>
2026-07-02 09:24:59 +0700

    update

--- lib/presentations/page/forgot password/controller/forgot_password_controller.dart
+++ lib/presentations/page/forgot password/controller/forgot_password_controller.dart
@@ -9,13 +9,13 @@ class ForgotPasswordController extends GetxController {
   }
 
   void submit() {
-    // 1. Cek apakah email kosong
+    // Cek apakah email kosong
     if (email.value.isEmpty) {
       AppSnackbar.error("Error", "Email wajib diisi");
       return;
     }
 
-    // 2. Cek apakah format email valid (otomatis mengecek ada @ dan domainnya)
+    // cek format email menggunakan GetUtils
     if (!GetUtils.isEmail(email.value)) {
       AppSnackbar.error("Error", "Format email tidak valid");
       return;
@@ -24,7 +24,7 @@ class ForgotPasswordController extends GetxController {
     // Jika lolos validasi, tampilkan pesan sukses
     AppSnackbar.success("Berhasil", "Link reset dikirim ke ${email.value}");
 
-    // 3. Kasih jeda 1.5 detik agar pop up terbaca, lalu kembali ke halaman login
+    // Kasih jeda 1.5 detik agar pop up terbaca, lalu kembali ke halaman login
     Future.delayed(const Duration(milliseconds: 1500), () {
       // Get.back();
       Get.offAllNamed('/login');
